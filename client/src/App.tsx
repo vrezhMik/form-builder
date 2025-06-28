7fb3f2aeab8489a707e2f94efca71a0dba3d2ce6
vrezhMikTumo <vrezh.mikauyelyan@tumo.org>
2025-06-28 16:20:53 +0400

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import HeaderComponent from "./components/Header/HeaderComponent";
+function App() {
+  return (
+    <>
+      <HeaderComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/HeaderComponent.tsx
@@ -0,0 +1,9 @@
+export default function HeaderComponent() {
+  return (
+    <header>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+        Click Me
+      </button>
+    </header>
+  );
+}
